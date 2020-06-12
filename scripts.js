029764f49fbfc112cdc2afec4a6e233f2dd519a7
atembamanu <atembaemm07@gmail.com>
2020-06-12 22:00:58 +0300

    Initial Commits

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// var circle = new circleDonutChart("projects");
+// circle.draw({
+//   end: 75,
+//   start: 0,
+//   maxValue: 100,
+//   outerCircleColor: "green",
+//   innerCircleColor: "#E9ECEF",
+//   size: 45,
+//   angleOfStart: -180,
+//   textColor: "#495057",
+// });
